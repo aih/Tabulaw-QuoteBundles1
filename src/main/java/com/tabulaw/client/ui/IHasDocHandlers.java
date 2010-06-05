3fe8cbe94c0f81f037117746c6c502cfeb467dec
jopaki <jopaki@gmail.com>
2010-06-04 23:46:39 -0700

    Forgot this in last commit.

---
+++ src/main/java/com/tabulaw/client/ui/IHasDocHandlers.java
@@ -0,0 +1,23 @@
+/**
+ * The Logic Lab
+ * @author jpk
+ * June 3, 2010
+ */
+package com.tabulaw.client.ui;
+
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.HasHandlers;
+
+/**
+ * Fires doc events.
+ * @author jpk
+ */
+public interface IHasDocHandlers extends HasHandlers {
+
+	/**
+	 * Adds a handler.
+	 * @param handler
+	 * @return {@link HandlerRegistration}
+	 */
+	HandlerRegistration addDocHandler(IDocHandler handler);
+}
