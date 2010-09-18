79db3e962a3f272623f2fcb13c87372d54c5e19e
unknown <Tabulaw@.(none)>
2010-09-18 14:19:58 +0200

    documents menu

---
+++ src/main/java/com/tabulaw/client/app/ui/DocumentsMenuBar.java
@@ -0,0 +1,62 @@
+package com.tabulaw.client.app.ui;
+
+import java.util.List;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.tabulaw.client.app.Poc;
+import com.tabulaw.client.app.model.ClientModelCache;
+import com.tabulaw.client.app.view.DocViewInitializer;
+import com.tabulaw.client.model.IModelChangeHandler;
+import com.tabulaw.client.model.ModelChangeEvent;
+import com.tabulaw.client.ui.Notifier;
+import com.tabulaw.client.view.ShowViewRequest;
+import com.tabulaw.client.view.ViewManager;
+import com.tabulaw.model.DocKey;
+import com.tabulaw.model.DocRef;
+import com.tabulaw.model.EntityType;
+
+public class DocumentsMenuBar extends MenuBar {
+
+	private final static DocumentsMenuBar MENU_BAR = new DocumentsMenuBar();
+
+	public DocumentsMenuBar() {
+		super(true);
+		update();
+		Poc.getPortal().addModelChangeHandler(new IModelChangeHandler() {
+			@Override
+			public void onModelChangeEvent(ModelChangeEvent event) {
+				update();
+			}
+		});
+	}
+
+	@SuppressWarnings("unchecked")
+	public void update() {
+		clearItems();
+		List<DocRef> docs = (List<DocRef>) ClientModelCache.get().getAll(
+				EntityType.DOCUMENT);
+		int count = 0;
+		for (final DocRef doc : docs) {
+			if (count++ >= 10) {
+				break;
+			}
+			addItem(doc.getName(), new Command() {
+				@Override
+				public void execute() {
+					if (ClientModelCache.get().getUserState()
+							.setCurrentQuoteBundleId(doc.getId())) {
+						Notifier.get().info("Current Document set.");
+						final DocKey docKey = doc.getModelKey();
+						final DocViewInitializer dvi = new DocViewInitializer(docKey);
+						ViewManager.get().dispatch(new ShowViewRequest(dvi));
+					}
+				}
+			});
+		}
+	}
+
+	public static MenuBar getDocumentsMenuBar() {
+		return MENU_BAR;
+	}
+}
