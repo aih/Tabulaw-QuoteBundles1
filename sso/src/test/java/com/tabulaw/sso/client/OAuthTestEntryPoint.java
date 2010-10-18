c049f56105c246abb0ca9d13ef77d104101b4783
Radek Olesiak <radek@olesiak.biz>
2010-10-18 18:52:36 +0200

    WEBAPP-5 OAuth

---
+++ sso/src/test/java/com/tabulaw/sso/client/OAuthTestEntryPoint.java
@@ -0,0 +1,9 @@
+package com.tabulaw.sso.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class OAuthTestEntryPoint implements EntryPoint{
+	@Override
+	public void onModuleLoad() {
+	}
+}
