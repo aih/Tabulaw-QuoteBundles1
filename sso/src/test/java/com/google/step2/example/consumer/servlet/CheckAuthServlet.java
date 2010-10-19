e6cc779606d6a2a86ba3ac967ca1e664b47230f6
Radek Olesiak <radek@imdapi.com>
2010-10-19 11:46:24 +0200

    WEBAPP-5 OAuth

--- sso/src/test/java/com/google/step2/example/consumer/servlet/CheckAuthServlet.java
+++ sso/src/test/java/com/google/step2/example/consumer/servlet/CheckAuthServlet.java
@@ -59,6 +59,7 @@ import javax.servlet.http.HttpSession;
  * @author Dirk Balfanz (dirk.balfanz@gmail.com)
  * @author Breno de Medeiros (breno.demedeiros@gmail.com)
  */
+
 @SuppressWarnings("serial")
 public class CheckAuthServlet extends InjectableServlet {
   private ConsumerHelper helper;
