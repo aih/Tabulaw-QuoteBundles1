afd1935de2605107976d29c6b70ac845288b06b9
Radek Olesiak <radek@imdapi.com>
2010-10-19 19:52:35 +0200

    WEBAPP-5

--- sso/src/test/java/com/tabulaw/sso/step2/client/Step2TestEntryPoint.java
+++ sso/src/test/java/com/tabulaw/sso/step2/client/Step2TestEntryPoint.java
@@ -1,8 +1,8 @@
-package com.tabulaw.sso.client;
+package com.tabulaw.sso.step2.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
-public class OAuthTestEntryPoint implements EntryPoint{
+public class Step2TestEntryPoint implements EntryPoint{
 	@Override
 	public void onModuleLoad() {
 	}
