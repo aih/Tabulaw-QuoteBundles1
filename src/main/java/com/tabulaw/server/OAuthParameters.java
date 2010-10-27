ed0ca1a5df2b0feb8e71a29246156f17efbbde88
Radek Olesiak <radek@imdapi.com>
2010-10-27 11:20:09 +0200

    WEBAPP-5 OAuth - parameters <-> HashMap; add to User

---
+++ src/main/java/com/tabulaw/server/OAuthParameters.java
@@ -0,0 +1,58 @@
+package com.tabulaw.server;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.gdata.client.authn.oauth.GoogleOAuthParameters;
+import com.google.gwt.dev.util.collect.HashMap;
+
+public class OAuthParameters extends GoogleOAuthParameters {
+
+	private final static Log log = LogFactory.getLog(OAuthParameters.class);
+
+	public OAuthParameters() {
+	}
+
+	public OAuthParameters(GoogleOAuthParameters init) {
+		fromMap(init.getBaseParameters());
+		fromMapExtra(init.getExtraParameters());
+	}
+
+	public OAuthParameters(Map<String, String> parameters,
+			Map<String, String> parametersExtra) {
+		fromMap(parameters);
+		fromMapExtra(parametersExtra);
+	}
+
+	public Map<String, String> toMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		for (Map.Entry<String, String> entry : getExtraParameters().entrySet()) {
+			map.put(entry.getKey(), entry.getValue());
+		}
+		return map;
+	}
+
+	public Map<String, String> toMapExtra() {
+		Map<String, String> map = new HashMap<String, String>();
+		for (Map.Entry<String, String> entry : getExtraParameters().entrySet()) {
+			map.put(entry.getKey(), entry.getValue());
+		}
+		return map;
+	}
+
+	public void fromMap(Map<String, String> map) {
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			log.debug("basic: " + entry.getKey() + " -> " + entry.getValue());
+			addCustomBaseParameter(entry.getKey(), entry.getValue());
+		}
+	}
+
+	public void fromMapExtra(Map<String, String> map) {
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			log.debug("extra: " + entry.getKey() + " -> " + entry.getValue());
+			addExtraParameter(entry.getKey(), entry.getValue());
+		}
+	}
+}
