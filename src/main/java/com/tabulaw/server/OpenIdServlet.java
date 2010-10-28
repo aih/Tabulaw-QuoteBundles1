baf3a9c03fc0e087735672d2ec88b95ea40a3ea5
Radek Olesiak <radek@imdapi.com>
2010-10-28 22:01:44 +0200

    WEBAPP-5 OpenID

--- src/main/java/com/tabulaw/server/OpenIdServlet.java
+++ src/main/java/com/tabulaw/server/OpenIdServlet.java
@@ -1,6 +1,7 @@
 package com.tabulaw.server;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +10,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openid4java.OpenIDException;
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
+import org.openid4java.message.AuthSuccess;
+import org.openid4java.message.ParameterList;
+import org.openid4java.message.ax.AxMessage;
+import org.openid4java.message.ax.FetchResponse;
 
 @SuppressWarnings("serial")
 public class OpenIdServlet extends HttpServlet {
 
 	private final static Log log = LogFactory.getLog(OpenIdServlet.class);
+
+	private final static String homeUrl = "/";
+	private final static String realm = "http://www.tabulaw.com/";
+
+	private ConsumerManager manager;
+
+	public OpenIdServlet() throws ConsumerException {
+		manager = new ConsumerManager();
+	}
 
 	/**
 	 * Either initiates a login to a given provider or processes a response from
@@ -37,5 +57,4 @@ public class OpenIdServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 	}
-
 }
