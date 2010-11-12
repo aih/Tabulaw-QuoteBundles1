07410c18ada5765456450e8f347cddfc3b57e491
Radek Olesiak <radek@imdapi.com>
2010-11-12 17:11:18 +0100

    * WEBAPP-75
    GWT webapp loading in the background on user login.

---
+++ src/main/java/com/tabulaw/server/jsp/LoginBean.java
@@ -0,0 +1,28 @@
+package com.tabulaw.server.jsp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.tabulaw.server.UserContext;
+
+public class LoginBean {
+
+	private HttpServletRequest request;
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public boolean isLoggedIn() {
+		if (request != null) {
+			HttpSession session = request.getSession(false);
+			return session.getAttribute(UserContext.KEY) != null;
+		} else {
+			return false;
+		}
+	}
+}
