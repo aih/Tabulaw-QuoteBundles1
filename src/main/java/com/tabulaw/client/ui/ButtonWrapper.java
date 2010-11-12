21da8b7a0679cc5121a340b04216a60e5d119e89
Radek Olesiak <radek@imdapi.com>
2010-11-12 17:09:43 +0100

    * WEBAPP-75
    GWT webapp loading in the background on user login.

---
+++ src/main/java/com/tabulaw/client/ui/ButtonWrapper.java
@@ -0,0 +1,14 @@
+package com.tabulaw.client.ui;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.Button;
+
+public class ButtonWrapper extends Button {
+	
+	public ButtonWrapper() {
+	}
+
+	public ButtonWrapper(Element e) {
+		super(e);
+	}
+}
