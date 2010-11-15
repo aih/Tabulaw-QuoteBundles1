cbf1fb5a332b24483df5e4dd0531e3d89fde2502
Radek Olesiak <radek@imdapi.com>
2010-11-15 11:15:33 +0100

    * WEBAPP-5 Add Google OpenID sign-in     * WEBAPP-81
    Add password generation for OpenID user registered

--- src/main/java/com/tabulaw/openid/UserSignIn.java
+++ src/main/java/com/tabulaw/openid/UserSignIn.java
@@ -1,6 +1,7 @@
 package com.tabulaw.openid;
 
 import java.io.IOException;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -37,7 +38,6 @@ public class UserSignIn {
 		return user;
 	}
 
-
 	private void reLoginUser(User user) throws IOException {
 		logout(req);
 		if (user != null) {
@@ -57,15 +57,39 @@ public class UserSignIn {
 		} catch (IllegalStateException e) {
 		}
 	}
+
 	private String getPasswordGenerated() {
-		// TODO change to random password and an user will reset the password
-		// later
-		return "openid";
+		return quickGeneratePassword();
 	}
 
 	private String getUserName(UserInfo user) {
 		String name = StringUtils.defaultString(user.getFirstName()) + " "
 				+ StringUtils.defaultString(user.getLastName());
 		return StringUtils.trim(name);
+	}
+
+	/** The random number generator. */
+	private static String quickGeneratePassword() {
+		Random r = new Random();
+		int MIN_LENGTH = 16;
+
+		/*
+		 * Set of characters that is valid. Must be printable, memorable, and
+		 * "won't break HTML" (i.e., not ' <', '>', '&', '=', ...). or break
+		 * shell commands (i.e., not ' <', '>', '$', '!', ...). I, L and O are
+		 * good to leave out, as are numeric zero and one.
+		 */
+		char[] goodChar = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',
+				'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+				'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M',
+				'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+				'2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '@', };
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < MIN_LENGTH; i++) {
+			sb.append(goodChar[r.nextInt(goodChar.length)]);
+		}
+
+		return sb.toString();
 	}
 }
