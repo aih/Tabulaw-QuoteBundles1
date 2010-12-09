178aa06dfec2937da3cf860133bd569b0d4be9c6
Radek Olesiak <radek@olesiak.biz>
2010-12-09 21:54:02 +0100

    add scripts in /etc for linux

---
+++ etc/installall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd org.springextensions.db4o/1.0.0e
+./installall.sh
+popd
+pushd gdata
+./installall.sh
+popd
+pushd step2
+./installall.sh
+popd
---
+++ etc/org.springextensions.db4o/1.0.0e/install.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=com.google.gdata -DartifactId=$1 -Dversion=$2 -Dfile=$3 -Dpackaging=jar -DgeneratePom=true
