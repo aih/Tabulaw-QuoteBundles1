5f6a670d014673220a1b160ff096613c11079f0e
Andrey Levchenko <levchenko.andrey@gmail.com>
2011-01-06 17:12:08 +0600

    [WEBAPP-97] Added 'on*' attributes removal

--- src/main/java/com/tabulaw/util/SafeHtmlUtils.java
+++ src/main/java/com/tabulaw/util/SafeHtmlUtils.java
@@ -3,15 +3,17 @@ package com.tabulaw.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
+import org.apache.commons.io.IOUtils;
 import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.SimpleHtmlSerializer;
 import org.htmlcleaner.TagNode;
-import org.apache.commons.io.IOUtils;
 
 public class SafeHtmlUtils {
 	private static String PRUNED_TAGS = "script,object,applet,form,embed,style,link,meta,iframe";
+	private static String ON_PREFIX = "on";
 	private static String[] PRUNED_ATTRS = new String[]{"class", "style", "href", "src", "background"};
 	
 	public static void sanitizeHtml(InputStream input, OutputStream output) throws IOException {
@@ -39,11 +41,19 @@ public class SafeHtmlUtils {
 		for(Object child : node.getChildren()) {
 			if (child instanceof TagNode) {
 				TagNode childTag=(TagNode) child;
+				//remove pruned attributes 
 				for (String attrName : PRUNED_ATTRS) {
 					if (childTag.hasAttribute(attrName)) {
 						childTag.removeAttribute(attrName);
 					}
 				}
+				//remove on* attributes 
+				for(String attrName : new ArrayList<String>(childTag.getAttributes().keySet())) {
+					if (attrName.startsWith(ON_PREFIX)) {
+						childTag.removeAttribute(attrName);
+					}
+				}
+				
 				cleanChilds(childTag);
 			}
 		}
