36efd61b985ca4edd2aeab5d524776f79c8f47c7
Ari Hershowitz <aih@tabulaw.com>
2011-04-07 14:03:52 -0700

    Modified mvn install scripts and pom.xml to upgrade to GWT 2.2.  I removed gwt-dev as an explicit dependency and changed the gwt-maven plugin to 2.2, in addition to changing the gwt jars themselves.

--- etc/gwt/installall.sh
+++ etc/gwt/installall.sh
@@ -1,2 +1,2 @@
-./install.sh com.allen_sauer.gwt.dnd gwt-dnd 3.0.1 gwt-dnd-3.0.1.jar
-./install.sh com.allen_sauer.gwt.log gwt-log 3.0.1 gwt-log-3.0.1.jar
+./install.sh com.allen_sauer.gwt.dnd gwt-dnd 3.1.0 gwt-dnd-3.1.0.jar
+./install.sh com.allen_sauer.gwt.log gwt-log 3.1.0 gwt-log-3.1.0.jar
