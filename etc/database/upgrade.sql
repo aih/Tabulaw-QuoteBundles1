64fdbdd07beebae7100b2ece5d3f6d87fa754abd
Maxim Kramarenko <maximkr@gmail.com>
2011-04-07 20:44:32 +0400

    not null issue

--- etc/database/upgrade.sql
+++ etc/database/upgrade.sql
@@ -3,3 +3,5 @@ alter table tw_permission drop column permission_orphanedquotebundle;
 alter table tw_userstate add column userstate_allquotebundle text;
 
 create unique index fki_userstate_allquotebundle on tw_userstate using btree (userstate_allquotebundle);
+
+ALTER TABLE tw_userstate ALTER COLUMN userstate_quotebundle DROP NOT NULL;
